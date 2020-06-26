5462c1f3e40f08582adbfb97a1ffe761055b6ac2
Anna Nicole Reed <reedanna@mail.com>
2020-06-26 20:05:13 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, person|
+    if suite == :suite_a && person[0] == "A"
+      winner = name
+    end
+  end
+  winner
 end
